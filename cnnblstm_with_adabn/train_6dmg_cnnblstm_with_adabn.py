e54073edebc03c9030d4c41b7ad208572200d4d3
Fassial <2508440340@qq.com>
2020-02-24 10:27:40 +0800

    cnnblstm_with_adabn: fix bugs in train_6dmg_cnnblstm_with_adabn.py

--- cnnblstm_with_adabn/train_6dmg_cnnblstm_with_adabn.py
+++ cnnblstm_with_adabn/train_6dmg_cnnblstm_with_adabn.py
@@ -19,16 +19,14 @@ if __name__ == '__main__':
 		m_cnnblstm_with_adabn = cnnblstm_with_adabn(use_cuda = 0, params_dir = "./params_6dmg")
 	print(m_cnnblstm_with_adabn)
 	# get train_x, train_y
-	Y, segments, maxlen_seg, n_files, seq_length = tools.getAllData(TRAIN_PATH)
-	X_all, y_all, _ = tools.transferData(Y, segments, n_files, seq_length)
+	X_all, y_all = tools_6dmg.preprocess(TRAIN_PATH)
 	train_x = torch.from_numpy(X_all)
 	train_y = torch.from_numpy(y_all)
 	train_data = torch.utils.data.TensorDataset(train_x, train_y)
 	# trainAllLayers
 	m_cnnblstm_with_adabn.trainAllLayers(train_data)
 	# get test_x, test_y
-	Y, segments, maxlen_seg, n_files, seq_length = tools.getAllData(TEST_PATH)
-	X_all, y_all, _ = tools.transferData(Y, segments, n_files, seq_length)
+	X_all, y_all = tools_6dmg.preprocess(TEST_PATH)
 	test_x = torch.from_numpy(X_all)
 	test_y = torch.from_numpy(y_all)
 	# get test accuracy
