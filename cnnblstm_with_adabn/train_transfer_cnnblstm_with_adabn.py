fb3accca358b9a9bc5caaf5a0266801488efc2d2
Fassial <2508440340@qq.com>
2020-02-26 15:38:56 +0800

    cnnblstm_with_adabn: support N_TRAINSET in train_transfer

--- cnnblstm_with_adabn/train_transfer_cnnblstm_with_adabn.py
+++ cnnblstm_with_adabn/train_transfer_cnnblstm_with_adabn.py
@@ -5,8 +5,8 @@ sys.path.append("..")
 import tools
 from transfer_cnnblstm_with_adabn import transfer_cnnblstm_with_adabn
 
-TRAIN_PATH = r"../dataset/transfer_train"
-TEST_PATH = r"../dataset/transfer_test"
+N_TRAINSET = 0
+TRANSFER_PATH = r"../dataset/transfer"
 
 if __name__ == '__main__':
 	torch.manual_seed(2)
@@ -19,18 +19,26 @@ if __name__ == '__main__':
 		print(use_cuda)
 		m_transfer_cnnblstm_with_adabn = transfer_cnnblstm_with_adabn(use_cuda = use_cuda)
 	print(m_transfer_cnnblstm_with_adabn)
-	# get train_x, train_y
-	Y, segments, maxlen_seg, n_files, seq_length = tools.getAllData(TRAIN_PATH)
-	X_all, y_all, _ = tools.transferData(Y, segments, n_files, seq_length)
-	train_x = torch.from_numpy(X_all)
-	train_y = torch.from_numpy(y_all)
+	# get transfer_x & transfer_y
+	Y, segments, maxlen_seg, n_files, seq_length = tools.getAllData(TRANSFER_PATH)
+	transfer_x, transfer_y, _ = tools.transferData(Y, segments, n_files, seq_length)
+	# get permutation index
+	per = np.random.permutation(transfer_x.shape[0])
+	# premute transfer_x & transfer_y
+	transfer_x = transfer_x[per, :, :]
+	transfer_y = transfer_y[per]
+	# get train_x & train_y
+	train_x = torch.from_numpy(transfer_x[:N_TRAINSET, :, :])
+	train_y = torch.from_numpy(transfer_y[:N_TRAINSET])
+	print(train_x.shape, train_y.shape)
+	# get test_x & test_y
+	test_x = torch.from_numpy(transfer_x[N_TRAINSET:, :, :])
+	test_y = torch.from_numpy(transfer_y[N_TRAINSET:])
+	print(test_x.shape, test_y.shape)
+	# get test accuracy
+	m_transfer_cnnblstm_with_adabn.getTestAccuracy(test_x, test_y)
 	# trainAllLayers
 	m_transfer_cnnblstm_with_adabn.trainAllLayers(train_x, train_y)
-	# get test_x, test_y
-	Y, segments, maxlen_seg, n_files, seq_length = tools.getAllData(TEST_PATH)
-	X_all, y_all, _ = tools.transferData(Y, segments, n_files, seq_length)
-	test_x = torch.from_numpy(X_all)
-	test_y = torch.from_numpy(y_all)
 	# get test accuracy
 	m_transfer_cnnblstm_with_adabn.getTestAccuracy(test_x, test_y)
 
