2b749871d5bfc923c9a2acf47ae75e87d6c83d0e
Fassial <2508440340@qq.com>
2020-02-27 12:40:59 +0800

    cnnblstm_with_adabn: python train_adabn train train_path params_path

--- cnnblstm_with_adabn/train_cnnblstm_with_adabn.py
+++ cnnblstm_with_adabn/train_cnnblstm_with_adabn.py
@@ -13,36 +13,41 @@ enable_PCA = True
 if __name__ == '__main__':
 	torch.manual_seed(2)
 
+	# get TRAIN_PATH_SYS & TEST_PATH_SYS & PARAMS_PATH_SYS
+	CMD = sys.argv[1]
+	PATH_SYS = sys.argv[2]
+	PARAMS_PATH_SYS = sys.argv[3]
 	# whether use cuda
 	use_cuda = torch.cuda.is_available()
 	if use_cuda:
-		m_cnnblstm_with_adabn = cnnblstm_with_adabn(use_cuda = 1).cuda()
+		m_cnnblstm_with_adabn = cnnblstm_with_adabn(use_cuda = use_cuda, params_dir = PARAMS_PATH_SYS).cuda()
 	else:
-		m_cnnblstm_with_adabn = cnnblstm_with_adabn(use_cuda = 0)
+		m_cnnblstm_with_adabn = cnnblstm_with_adabn(use_cuda = use_cuda, params_dir = PARAMS_PATH_SYS)
 	print(m_cnnblstm_with_adabn)
-	# get TRAIN_PATH_SYS & TEST_PATH_SYS
-	TRAIN_PATH_SYS = sys.argv[1]
-	TEST_PATH_SYS = sys.argv[2]
-	# get train_x, train_y
-	Y, segments, maxlen_seg, n_files, seq_length = tools.getAllData(TRAIN_PATH_SYS)
-	train_x, train_y, _ = tools.transferData(Y, segments, n_files, seq_length)
-	# enable PCA
-	if enable_PCA:
-		train_x = tools.PCA_Xs(train_x).astype(np.float32)
-		print(train_x.shape)
-	train_x = torch.from_numpy(train_x)
-	train_y = torch.from_numpy(train_y)
-	# trainAllLayers
-	m_cnnblstm_with_adabn.trainAllLayers(train_x, train_y)
-	# get test_x, test_y
-	Y, segments, maxlen_seg, n_files, seq_length = tools.getAllData(TEST_PATH_SYS)
-	test_x, test_y, _ = tools.transferData(Y, segments, n_files, seq_length)
-	# enable PCA
-	if enable_PCA:
-		test_x = tools.PCA_Xs(test_x).astype(np.float32)
-		print(test_x.shape)
-	test_x = torch.from_numpy(test_x)
-	test_y = torch.from_numpy(test_y)
-	# get test accuracy
-	m_cnnblstm_with_adabn.getTestAccuracy(test_x, test_y)
+	if (CMD == "train"):
+		# get train_x, train_y
+		Y, segments, maxlen_seg, n_files, seq_length = tools.getAllData(PATH_SYS)
+		train_x, train_y, _ = tools.transferData(Y, segments, n_files, seq_length)
+		# enable PCA
+		if enable_PCA:
+			train_x = tools.PCA_Xs(train_x).astype(np.float32)
+			print(train_x.shape)
+		train_x = torch.from_numpy(train_x)
+		train_y = torch.from_numpy(train_y)
+		# trainAllLayers
+		m_cnnblstm_with_adabn.trainAllLayers(train_x, train_y)
+	elif (CMD == "test"):
+		# get test_x, test_y
+		Y, segments, maxlen_seg, n_files, seq_length = tools.getAllData(PATH_SYS)
+		test_x, test_y, _ = tools.transferData(Y, segments, n_files, seq_length)
+		# enable PCA
+		if enable_PCA:
+			test_x = tools.PCA_Xs(test_x).astype(np.float32)
+			print(test_x.shape)
+		test_x = torch.from_numpy(test_x)
+		test_y = torch.from_numpy(test_y)
+		# get test accuracy
+		m_cnnblstm_with_adabn.getTestAccuracy(test_x, test_y)
+	else:
+		print("CMD ERROR!")
 
