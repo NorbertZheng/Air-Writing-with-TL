643252b3647398359ada02c3a419bc5e6577a752
Fassial <2508440340@qq.com>
2020-02-27 12:27:49 +0800

    cnnblstm: fix bugs in train_6dmg

--- cnnblstm/train_6dmg_cnnblstm.py
+++ cnnblstm/train_6dmg_cnnblstm.py
@@ -19,19 +19,23 @@ if __name__ == '__main__':
 		cnnblstm = cnnblstm(params_file = "./params_6dmg.pkl", use_cuda = use_cuda)
 	print(cnnblstm)
 	# get TRAIN_PATH_SYS & TEST_PATH_SYS
-	TRAIN_PATH_SYS = sys.argv[1]
-	TEST_PATH_SYS = sys.argv[2]
-	# get train_x, train_y
-	X_all, y_all = tools_6dmg.preprocess(TRAIN_PATH_SYS)
-	print(X_all.shape, y_all.shape)
-	train_x = torch.from_numpy(X_all)
-	train_y = torch.from_numpy(y_all)
-	# trainAllLayers
-	cnnblstm.trainAllLayers(train_x, train_y)
-	# get test_x, test_y
-	X_all, y_all = tools_6dmg.preprocess(TEST_PATH_SYS)
-	test_x = torch.from_numpy(X_all)
-	test_y = torch.from_numpy(y_all)
-	# get test accuracy
-	cnnblstm.getTestAccuracy(test_x, test_y)
+	CMD = sys.argv[1]
+	PATH_SYS = sys.argv[2]
+	if (CMD == "train"):
+		# get train_x, train_y
+		X_all, y_all = tools_6dmg.preprocess(PATH_SYS)
+		print(X_all.shape, y_all.shape)
+		train_x = torch.from_numpy(X_all)
+		train_y = torch.from_numpy(y_all)
+		# trainAllLayers
+		cnnblstm.trainAllLayers(train_x, train_y)
+	elif (CMD == "test"):
+		# get test_x, test_y
+		X_all, y_all = tools_6dmg.preprocess(PATH_SYS)
+		test_x = torch.from_numpy(X_all)
+		test_y = torch.from_numpy(y_all)
+		# get test accuracy
+		cnnblstm.getTestAccuracy(test_x, test_y)
+	else:
+		print("CMD ERROR!")
 
