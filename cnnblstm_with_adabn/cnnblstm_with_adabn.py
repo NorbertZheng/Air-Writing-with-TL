a92b3f8e1e55b1fe30d18728bcec5beaf2c84e71
Fassial <2508440340@qq.com>
2020-03-11 18:55:28 +0800

    cnnblstm_with_adabn: 64-100

--- cnnblstm_with_adabn/cnnblstm_with_adabn.py
+++ cnnblstm_with_adabn/cnnblstm_with_adabn.py
@@ -33,9 +33,9 @@ class cnnblstm_with_adabn(nn.Module):
 			os.mkdir(self.params_dir)
 		self.enable_CORAL = enable_CORAL
 
-		self.n_filters = 32
+		self.n_filters = 64
 		self.kernel_size = 15
-		self.n_hidden = 50	# 150
+		self.n_hidden = 100	# 150
 		self.n_layers = 1
 		self.bidirectional = True
 
